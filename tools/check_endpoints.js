e6db64574ddd19f627ec243ee60af8293b63254a
omarsameh-in <omar.sameh.internway@gmail.com>
2026-06-18 08:23:57 +0300

    Point backend to https://rd6j07gh-5022.uks1.devtunnels.ms

---
+++ tools/check_endpoints.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const http = require('http');
+
+function head(url){
+  return new Promise((resolve) => {
+    const lib = url.startsWith('https') ? https : http;
+    try {
+      const req = lib.request(url, { method: 'HEAD', timeout: 10000 }, (res) => {
+        console.log(url, 'STATUS', res.statusCode);
+        resolve();
+      });
+      req.on('error', (e) => { console.log(url, 'ERR', e.message); resolve(); });
+      req.on('timeout', () => { console.log(url, 'ERR', 'timeout'); req.destroy(); resolve(); });
+      req.end();
+    } catch (e) {
+      console.log(url, 'ERR', e.message);
+      resolve();
+    }
+  });
+}
+
+(async () => {
+  await head('https://rd6j07gh-5022.uks1.devtunnels.ms/swagger/index.html');
+  await head('https://rd6j07gh-5022.uks1.devtunnels.ms/api/Notifications');
+  await head('http://localhost:3000/api/Notifications');
+})();
